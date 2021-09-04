52108d36bd8c1bdbda69effa4c7042090369847f
ChandruSundar <chandrus2611@gmail.com>
2021-09-04 20:22:23 +0530

    New update on cucumber

--- src/test/java/org/telecom/stepdefinition/AddCustomer.java
+++ src/test/java/org/telecom/stepdefinition/AddCustomer.java
@@ -1,5 +1,7 @@
 package org.telecom.stepdefinition;
 
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
@@ -11,6 +13,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.cucumber.datatable.DataTable;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AddCustomer {
@@ -45,7 +48,46 @@ public class AddCustomer {
 		driver.findElement(By.xpath("//input[@placeholder='Mobile Number']")).sendKeys("9999999999");
 		
 	}
-
+	
+	@When("user needs to fill up the details by {int} dimensional list")
+	public void user_needs_to_fill_up_the_details_by_dimensional_list(Integer int1, DataTable datas) {
+	   
+		List<String> details = datas.asList(String.class);
+		
+		driver.findElement(By.xpath("//label[text()='Done']")).click();
+		driver.findElement(By.xpath("//input[@placeholder='FirstName']")).sendKeys(details.get(0));
+		driver.findElement(By.xpath("//input[@placeholder='LastName']")).sendKeys(details.get(1));
+		driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys(details.get(2));
+		driver.findElement(By.xpath("//textarea[@placeholder='Enter your address']")).sendKeys(details.get(3));
+		driver.findElement(By.xpath("//input[@placeholder='Mobile Number']")).sendKeys(details.get(4));
+	}
+	
+	@When("user needs to fill up the details by {int} dimensional map")
+	public void user_needs_to_fill_up_the_details_by_dimensional_map(Integer int1, DataTable datas) {
+	   
+		Map<String, String> detailsmap = datas.asMap(String.class, String.class);
+		
+		
+		driver.findElement(By.xpath("//label[text()='Done']")).click();
+		driver.findElement(By.xpath("//input[@placeholder='FirstName']")).sendKeys(detailsmap.get("fname"));
+		driver.findElement(By.xpath("//input[@placeholder='LastName']")).sendKeys(detailsmap.get("Lname"));
+		driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys(detailsmap.get("mail"));
+		driver.findElement(By.xpath("//textarea[@placeholder='Enter your address']")).sendKeys(detailsmap.get("addr"));
+		driver.findElement(By.xpath("//input[@placeholder='Mobile Number']")).sendKeys(detailsmap.get("phno"));
+	}
+	
+	@When("user needs to fill up the details {string}{string}{string}{string}{string}")
+	public void user_needs_to_fill_up_the_details(String fname, String lname, String mail, String addr, String phno) {
+	    
+		driver.findElement(By.xpath("//label[text()='Done']")).click();
+		driver.findElement(By.xpath("//input[@placeholder='FirstName']")).sendKeys(fname);
+		driver.findElement(By.xpath("//input[@placeholder='LastName']")).sendKeys(lname);
+		driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys(mail);
+		driver.findElement(By.xpath("//textarea[@placeholder='Enter your address']")).sendKeys(addr);
+		driver.findElement(By.xpath("//input[@placeholder='Mobile Number']")).sendKeys(phno);
+		
+	}
+	
 	@When("user clicks on submit button")
 	public void user_clicks_on_submit_button() {
 		
